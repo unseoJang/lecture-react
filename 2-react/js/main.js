b7c8efebc86b2c93948466079e8a0dda4ddc5fda
unseo Jang <unseojang@unseoui-MacBookPro-3.local>
2023-12-11 20:40:51 +0900

    [react/search-from-2] 검색 창 클래스 문법에 추가

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,16 +1,20 @@
 // @ts-nocheck
-const element = (
-	<>
-		<header>
-			<h2 className="container">검색</h2>
-		</header>
-		<div className="container">
-			<form id="search-form-view">
-				<input type="text" placeholder="검색어를 입력하세요" autoFocus />
-				<button type="reset" className="btn-reset"></button>
-			</form>
-		</div>
-	</>
-);
+class App extends React.component {
+	render() {
+		return (
+			<>
+				<header>
+					<h2 className="container">검색</h2>
+				</header>
+				<div className="container">
+					<form id="search-form-view">
+						<input type="text" placeholder="검색어를 입력하세요" autoFocus />
+						<button type="reset" className="btn-reset"></button>
+					</form>
+				</div>
+			</>
+		);
+	}
+}
 
-ReactDom.render(element, document.querySelector("#app"));
+ReactDom.render(<App />, document.querySelector("#app"));
