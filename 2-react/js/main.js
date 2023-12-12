6b2fa065bd357eb7a66c663d428e82e203314d70
unseo Jang <unseojang@unseoui-MacBookPro-3.local>
2023-12-12 20:53:30 +0900

    [react/search-from-3] value 추가

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -7,6 +7,17 @@ class App extends React.component {
 			searchKeyword: "",
 		};
 	}
+
+	handleChangeInput(event) {
+		// TODO
+		// this.state.searchKeyword = event.target.value;
+		// this.forceUpdate();
+
+		// 컴포넌트 상태 변화 약속
+		this.setState({
+			searchKeyword: event.target.value,
+		});
+	}
 	render() {
 		return (
 			<>
@@ -20,6 +31,8 @@ class App extends React.component {
 							placeholder="검색어를 입력하세요"
 							autoFocus
 							value={this.state.searchKeyword}
+							// TODO
+							onChange={(event) => this.handleChangeInput(event)}
 						/>
 						<button type="reset" className="btn-reset"></button>
 					</form>
